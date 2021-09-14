1eebff6b92d890cf8cfa760ed564f92489e8b807
Matejsmycka <45017046+Matejsmycka@users.noreply.github.com>
2021-09-14 11:27:08 -0400

    Add files via upload

---
+++ URLs.py
@@ -0,0 +1,8 @@
+cookies = {
+    'iscreds': 'VotiEWh1syLlZUdXXXXXXXX',
+    'issession': 'vYhXf9b0jKj47OZXXXXXXXX',
+ }
+
+
+urls=[
+"https://is.muni.cz/auth/seminare/student?fakulta=1433;obdobi=8403;studium=1073518;predmet=1405689;prihlasit=646196;akce=podrob;provest=1;stopwindow=1;design=m"]
---
+++ __main__.py
@@ -0,0 +1,55 @@
+import json
+import requests
+import re
+import URLs
+import time
+import datetime
+
+def main(): 
+
+ 
+   def prihlaseni():
+    for i in URLs.urls:
+       try:
+          response = requests.get(i,  cookies=URLs.cookies) 
+       except HTTPError as http_err:
+          print(f'HTTP error occurred: {http_err}')
+       except Exception as err:
+          print(f'Other error occurred: {err}')
+       else:
+          print("Zadost byla podana")
+          
+       obsah= str(response.content)
+       string= "semin.xc3.xa1rn.xc3.xad skupiny p.xc5.x99ihl.xc3.xa1.xc5.xa1en.a"
+       
+       try:
+          search = re.search(string,obsah).group()
+          if search:
+             print("Prihlaseno nebo nezmeneno")
+       except: 
+             print("NEPRIHLASENO")
+             prihlaseni()
+
+   def odpocet():
+    Check = False
+    
+    while Check == False:
+     t = time.localtime()
+     current_time = time.strftime("%H%M%S", t)  
+     current_time = int(current_time)+60000      
+     print(current_time)   
+      
+     if current_time >= 134000:
+       prihlaseni()
+       Check = True 
+     
+    
+   odpocet()      
+   
+     
+   
+if __name__ == '__main__':
+    main()
+    
+    
+    
